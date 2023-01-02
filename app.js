d52feacbce3485b636377b1e0e625a4f8e4327ee
Chemical7 <brianleputu@gmail.com>
2023-01-02 11:00:23 +0200

    add: API logic to fetch photos

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+// Unsplash API
+const count = 10;
+const apiKey = "YmSdlD5coaQD_omfsatbJjzSsP0aKBMEi-OvRJrdAH4";
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+
+// Get photos from API
+async function getPhotosFromAPI() {
+  try {
+    const response = await fetch(apiURL);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    // Catch error here
+  }
+}
+
+// On load get photos
+getPhotos();
